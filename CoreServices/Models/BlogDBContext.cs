c56144eba27eea72665bceaa4fd8da70137b88fb
DESKTOP-OKCFP03\durga <durga123us@gmail.com>
2019-12-01 23:56:50 +0530

    Add project files.

---
+++ CoreServices/Models/BlogDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoreServices.Models
+{
+    public partial class BlogDBContext : DbContext
+    {
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:mysqlserver1511.database.windows.net,1433;Initial Catalog=BlogDB;Persist Security Info=False;User ID=durgayojit1511;Password=Yes7boss@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("NAME")
+                    .HasColumnType("varchar(255)");
+
+                entity.Property(e => e.Slug)
+                    .HasColumnName("SLUG")
+                    .HasColumnType("varchar(255)");
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.PostId).HasColumnName("POST_ID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CATEGORY_ID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("CREATED_DATE")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("DESCRIPTION")
+                    .HasColumnType("varchar(max)");
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("TITLE")
+                    .HasColumnType("varchar(2000)");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__Post__CATEGORY_I__1273C1CD");
+            });
+        }
+    }
+}
---
+++ CoreServices/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CoreServices.Models;
+using CoreServices.Repository;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CoreServices
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(option => option.AddPolicy("MyBlogPolicy", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+
+            }));
+
+          
+
+            // Add framework services.
+            services.AddMvc();
+            services.AddDbContext<BlogDBContext>(item => item.UseSqlServer(Configuration.GetConnectionString("BlogDBConnection")));
+            services.AddScoped<IPostRepository, PostRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "API's hosted in Azure with Azure SQL",
+                    Description = "Test API with ASP.NET Core",
+                    TermsOfService = "None",
+                    Contact = new Contact()
+                    {
+                        Name = "Dotnet Detail",
+                        Email = "duraraopanduru15@gmail.com",
+                        Url = "www.dotnetdetail.net"
+                    },
+                    License = new License
+                    {
+                        Name = "Durga Rao Panduru",
+                        Url = "www.dotnetdetail.net"
+                    },
+                });
+
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseCors("MyBlogPolicy");
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+            });
+        }
+    }
+}
